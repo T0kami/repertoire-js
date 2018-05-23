2736b0bc14c2db0a661b1053ee4bb0fa24677a6d
T0kami <nmourolin@gmail.com>
2018-05-24 00:05:37 +0200

    CSS improvements

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 function main(){
+  String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+  }
   //Création de la BDD
   var contactListDiv = document.getElementById("contactList");
   var db;
@@ -16,26 +19,26 @@ function main(){
 
   request.onupgradeneeded = function() {
 
-        db = request.result;
-        function createTable(){
-          var objectStore = db.createObjectStore("contacts", {autoIncrement: true});
+    db = request.result;
+    function createTable(){
+      var objectStore = db.createObjectStore("contacts", {autoIncrement: true});
 
-          objectStore.createIndex("name", "name", { unique: false });
-          objectStore.createIndex("phone", "phone", { unique: true });
-          objectStore.createIndex("email", "email", { unique: false });
+      objectStore.createIndex("name", "name", { unique: false });
+      objectStore.createIndex("phone", "phone", { unique: true });
+      objectStore.createIndex("email", "email", { unique: false });
 
-        }
+    }
 
-        // On ajoute un magasin d'objet à la base de données
-        if (!db.objectStoreNames.contains('contacts')) {
+    // On ajoute un magasin d'objet à la base de données
+    if (!db.objectStoreNames.contains('contacts')) {
 
-          createTable();
+      createTable();
 
-        }else{
-          db.deleteObjectStore("contacts");
+    }else{
+      db.deleteObjectStore("contacts");
 
-          createTable();
-        }
+      createTable();
+    }
 
 
   }
@@ -43,88 +46,88 @@ function main(){
 
   function add(contactInfos) {
 
-         var request = db.transaction(["contacts"], "readwrite").objectStore("contacts").add(contactInfos);
+    var request = db.transaction(["contacts"], "readwrite").objectStore("contacts").add(contactInfos);
 
-         request.onsuccess = function(event) {
-                 console.log("Le contact à bien été ajouté !");
+    request.onsuccess = function(event) {
+      alert("Le contact à bien été ajouté !");
 
-                 listAll(contactListDiv);
-         };
+      listAll(contactListDiv);
+    };
 
-         request.onerror = function(event) {
-            if (String(request.error).includes("uniqueness")){
-              alert("Ce numéro existe déjà dans votre répertoire !");
-            }else{
-                 console.log(request.error);
-            }
-         }
+    request.onerror = function(event) {
+      if (String(request.error).includes("uniqueness")){
+        alert("Ce numéro existe déjà dans votre répertoire !");
+      }else{
+        console.log(request.error);
+      }
+    }
 
   }
 
 
 
- function listAll(contactListDiv) {
-   var objectStore = db.transaction("contacts").objectStore("contacts");
+  function listAll(contactListDiv) {
+    var objectStore = db.transaction("contacts").objectStore("contacts");
+    contactListDiv.innerHTML = "";
+    objectStore.index('name').openCursor().onsuccess = function(event) {
+      var cursor = event.target.result;
+      if (cursor) {
+        contactListDiv.innerHTML += ("<div><h2>" + cursor.value.name + "</h2><p>" + cursor.value.phone + "</p><p>" + cursor.value.email + "</p></div>");
 
-   objectStore.index('name').openCursor().onsuccess = function(event) {
-     var cursor = event.target.result;
-     if (cursor) {
-           contactListDiv.innerHTML += ("<div>" + cursor.value.name + ", Téléphone: " + cursor.value.phone + ", Email: " + cursor.value.email + "</div>");
+        cursor.continue();
+      }
+    };
+  }
 
-           cursor.continue();
-     }
-   };
- }
+  function search(searchString, contactListDiv){
+    var objectStore = db.transaction("contacts").objectStore("contacts");
 
- function search(searchString, contactListDiv){
-   var objectStore = db.transaction("contacts").objectStore("contacts");
-   contactListDiv.innerHTML = "";
-   objectStore.index('name').openCursor().onsuccess = function(event) {
-     var cursor = event.target.result;
+    objectStore.index('name').openCursor().onsuccess = function(event) {
+      var cursor = event.target.result;
 
-     if (cursor) {
-       var response = cursor.value.name + " " + cursor.value.phone + " " + cursor.value.email ;
-       if(response.includes(searchString)){
-       //console.log(typeof(response));
+      if (cursor) {
+        var response = (cursor.value.name + " " + cursor.value.phone + " " + cursor.value.email).toLowerCase() ;
+        if(response.includes(searchString.toLowerCase())){
+          //console.log(typeof(response));
 
-       contactListDiv.innerHTML += ("<div>" + cursor.value.name + ", Téléphone: " + cursor.value.phone + ", Email: " + cursor.value.email + "</div>");
-       }
-      cursor.continue();
+          contactListDiv.innerHTML += ("<div><h2>" + cursor.value.name + "</h2><p>" + cursor.value.phone + "</p><p>" + cursor.value.email + "</p></div>");
+        }
+        cursor.continue();
       }
 
 
-   };
+    };
 
- }
+  }
 
- function clearData() {
-   // ouvre une transaction de lecture / écriture  prête pour le nettoyage
-   var transaction = db.transaction(["contacts"], "readwrite");
+  function clearData() {
+    // ouvre une transaction de lecture / écriture  prête pour le nettoyage
+    var transaction = db.transaction(["contacts"], "readwrite");
 
-   // en cas de succès de l'ouverture de la transaction
-   transaction.oncomplete = function(event) {
- 	   console.log("Transaction effectuée !");
-   };
+    // en cas de succès de l'ouverture de la transaction
+    transaction.oncomplete = function(event) {
+      console.log("Transaction effectuée !");
+    };
 
-   // en cas d'échec de l'ouverture de la transaction
-   transaction.onerror = function(event) {
+    // en cas d'échec de l'ouverture de la transaction
+    transaction.onerror = function(event) {
       console.log('Transaction en échec à cause de l\'erreur : ' + transaction.error);
-   };
+    };
 
-   // ouvre l'accès au un magasin "toDoList" de la transaction
-   var objectStore = transaction.objectStore("contacts");
+    // ouvre l'accès au un magasin "toDoList" de la transaction
+    var objectStore = transaction.objectStore("contacts");
 
-   // Vide le magasin d'objet
-   var objectStoreRequest = objectStore.clear();
+    // Vide le magasin d'objet
+    var objectStoreRequest = objectStore.clear();
 
-   objectStoreRequest.onsuccess = function(event) {
-     console.log("La base a bien été nettoyée !")
-   };
- };
+    objectStoreRequest.onsuccess = function(event) {
+      console.log("La base a bien été nettoyée !")
+    };
+  }
 
- document.querySelectorAll("input[name=search]")[0].addEventListener("keyup", function(event){
-   search(event.target.value, contactListDiv);
- });
+  document.querySelectorAll("input[name=search]")[0].addEventListener("keyup", function(event){
+    search(event.target.value, contactListDiv);
+  });
 
   //contacts.init("test");
 
@@ -136,12 +139,27 @@ function main(){
     for (let item of contactForm){
 
       if (item.type == "text"){
-        contactInfos[item.id] = item.value;
+        if (item.id == "name"){
+          var value = (item.value).capitalize();
+          contactInfos[item.id] = value;
+        }else contactInfos[item.id] = item.value;
       }
     }
     add(contactInfos);
   });
 
+  document.getElementById("delete").addEventListener("click", function(event){
+    var r = confirm("Voulez-vous vraiment supprimer tous les élèves ?");
+    if (r == true) {
+      clearData();
+      listAll(contactListDiv);
+      alert("Contacts supprimés !");
+    } else {
+      alert("Suppression annulée !");
+    }
+
+
+  });
 }
 
 main();
@@ -151,15 +169,15 @@ main();
 
 //Script pour les onglets
 function contactTabs(evt, onglet) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(onglet).style.display = "block";
-    evt.currentTarget.className += " active";
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(onglet).style.display = "flex";
+  evt.currentTarget.className += " active";
 }
